329953c7f76d37e4f7edbd9d0d1493a07cf00eba
Meilianum <164498186+Meilianum@users.noreply.github.com>
2024-04-18 04:00:25 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+import inquirer from "inquirer";
+let myBalance = 20000; // dollar
+let mypin = 2233;
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "pin",
+        message: "Enter your pin",
+        type: "number",
+    },
+]);
+if (pinAnswer === "mypin") {
+    console.log('correct pin code !!!');
+}
+let operationAns = await inquirer.prompt([
+    { name: "operation",
+        message: 'please selcet the option',
+        type: "list",
+        choices: ["withdraw", "checkbalance", "insufficantbalance"]
+    }
+]);
+console.log(operationAns.operation);
+if (operationAns.operation === "withdraw") {
+    let amountAns = await inquirer.prompt([
+        {
+            name: "amount",
+            message: "Enter your amount",
+            type: "number"
+        }
+    ]);
+    myBalance -= amountAns.amount;
+    console.log("your remaning balance is :" + myBalance);
+}
+else if (operationAns.operation) {
+    console.log('your balance is' + myBalance);
+}
+;
